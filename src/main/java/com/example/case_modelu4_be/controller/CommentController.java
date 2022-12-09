09eaa109feee49d9ed0042b6404266fe71847eba
CuDinh03 <dinhhuycu0305@gmail.com>
2022-12-09 10:03:20 +0700

    cu

---
+++ src/main/java/com/example/case_modelu4_be/controller/CommentController.java
@@ -0,0 +1,4 @@
+package com.example.case_modelu4_be.controller;
+
+public class CommentController {
+}
---
+++ src/main/java/com/example/case_modelu4_be/model/Feedback.java
@@ -0,0 +1,17 @@
+package com.example.case_modelu4_be.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Feedback {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String content;
+    @ManyToOne
+    private Shop shop;
+}
