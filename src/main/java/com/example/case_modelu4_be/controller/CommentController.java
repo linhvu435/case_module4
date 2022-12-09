c801c1702fbb6c432158ee7c9bb40452fcb8f5e6
CuDinh03 <dinhhuycu0305@gmail.com>
2022-12-10 00:22:34 +0700

    day 2

--- src/main/java/com/example/case_modelu4_be/controller/CommentController.java
+++ src/main/java/com/example/case_modelu4_be/controller/CommentController.java
@@ -1,9 +1,8 @@
 package com.example.case_modelu4_be.controller;
 
 import com.example.case_modelu4_be.model.Comment;
-import com.example.case_modelu4_be.model.Evaluate;
 import com.example.case_modelu4_be.repository.ICommentRepo;
-import com.example.case_modelu4_be.repository.IEvaluate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,13 +10,14 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/comment")
+@RequestMapping("/comments")
 public class CommentController {
 
     @Autowired
     ICommentRepo iCommentRepo;
+
     @PostMapping
-    public ResponseEntity save(@RequestBody Comment comment){
+    public ResponseEntity save(@RequestBody Comment comment) {
         iCommentRepo.save(comment);
         return new ResponseEntity(HttpStatus.OK);
     }
