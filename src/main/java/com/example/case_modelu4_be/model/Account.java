d6cbf09ee9313fb14728e9a248d11a5d100c910b
hoangquangngoc <hoangquangngoc10@gmail.com>
2022-12-13 14:40:53 +0700

    ngoc3

--- src/main/java/com/example/case_modelu4_be/model/Account.java
+++ src/main/java/com/example/case_modelu4_be/model/Account.java
@@ -24,7 +24,7 @@ public class Account {
     private String passWord;
     @Pattern(regexp = "(84|03|07|08|05)+([0-9]{8})\\b" , message = "Số điện thoại bắt đầu bằng 84")
     private String phoneNumber;
-
+    @Email
     private String email;
     @Column(columnDefinition = "nvarchar(800)")
     private String address;
